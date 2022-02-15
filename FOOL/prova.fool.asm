ba381e2e87ae25e61e84c4a314ff83abb1e7386e
Veronika Folin <veronika.folin@studio.unibo.it>
2022-02-15 15:34:04 +0100

    Added new operator methods in CodeGenerationASTVisitor

---
+++ FOOL/prova.fool.asm
@@ -0,0 +1,14 @@
+push 0
+push 6
+push 1
+lfp
+push -2
+add
+lw
+lfp
+push -3
+add
+lw
+sub
+print
+halt
--- FOOL/src/compiler/CodeGenerationASTVisitor.java
+++ FOOL/src/compiler/CodeGenerationASTVisitor.java
@@ -114,6 +114,40 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	}
 
 	@Override
+	public String visitNode(LessEqualNode n) {
+		if (print) printNode(n);
+		String l1 = freshLabel();
+		String l2 = freshLabel();
+		return nlJoin(
+				visit(n.left),
+				visit(n.right),
+				"bleq "+l1,
+				"push 0",
+				"b "+l2,
+				l1+":",
+				"push 1",
+				l2+":"
+		);
+	}
+
+	@Override
+	public String visitNode(GreaterEqualNode n) {
+		if (print) printNode(n);
+		String l1 = freshLabel();
+		String l2 = freshLabel();
+		return nlJoin(
+				visit(n.right),
+				visit(n.left),
+				"bleq "+l1,
+				"push 0",
+				"b "+l2,
+				l1+":",
+				"push 1",
+				l2+":"
+		);
+	}
+
+	@Override
 	public String visitNode(TimesNode n) {
 		if (print) printNode(n);
 		return nlJoin(
@@ -124,12 +158,89 @@ public class CodeGenerationASTVisitor extends BaseASTVisitor<String, VoidExcepti
 	}
 
 	@Override
+	public String visitNode(DivNode n) {
+		if (print) printNode(n);
+		return nlJoin(
+				visit(n.left),
+				visit(n.right),
+				"div"
+		);
+	}
+
+	@Override
 	public String visitNode(PlusNode n) {
 		if (print) printNode(n);
 		return nlJoin(
 			visit(n.left),
 			visit(n.right),
 			"add"				
+		);
+	}
+
+	@Override
+	public String visitNode(MinusNode n) {
+		if (print) printNode(n);
+		return nlJoin(
+				visit(n.left),
+				visit(n.right),
+				"sub"
+		);
+	}
+
+	@Override
+	public String visitNode(NotNode n) {
+		if (print) printNode(n);
+		String l1 = freshLabel();
+		String l2 = freshLabel();
+		return nlJoin(
+				visit(n.exp),
+				"push 1",
+				"beq "+l1,
+				"push 1",
+				"b "+l2,
+				l1+":",
+				"push 0",
+				l2+":"
+		);
+	}
+
+	@Override
+	public String visitNode(OrNode n) {
+		if (print) printNode(n);
+		String l1 = freshLabel();
+		String l2 = freshLabel();
+		return nlJoin(
+				visit(n.left),
+				"push 1",
+				"beq "+l1,
+				visit(n.right),
+				"push 1",
+				"beq "+l1,
+				"push 0",
+				"b "+l2,
+				l1+":",
+				"push 1",
+				l2+":"
+		);
+	}
+
+	@Override
+	public String visitNode(AndNode n) {
+		if (print) printNode(n);
+		String l1 = freshLabel();
+		String l2 = freshLabel();
+		return nlJoin(
+				visit(n.left),
+				"push 0",
+				"beq "+l1,
+				visit(n.right),
+				"push 0",
+				"beq "+l1,
+				"push 1",
+				"b "+l2,
+				l1+":",
+				"push 0",
+				l2+":"
 		);
 	}
 
