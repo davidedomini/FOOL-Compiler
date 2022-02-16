195dc1843b9cdaf17302cf43d4305d915b046c64
Veronika Folin <veronika.folin@studio.unibo.it>
2022-02-16 10:47:24 +0100

    Added new classes in AST to manage object-orientated extention

--- FOOL/src/compiler/AST.java
+++ FOOL/src/compiler/AST.java
@@ -234,4 +234,118 @@ public class AST {
 		@Override
 		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
 	}
+
+	// OBJECT-ORIENTED EXTENSION
+
+	public static class ClassNode extends DecNode{
+		final List<FieldNode> fields;
+		final List<MethodNode> methods;
+		final String superID;
+		ClassNode(List<FieldNode> f, List<MethodNode> m, String id, TypeNode t) {
+			fields = Collections.unmodifiableList(f);
+			methods = Collections.unmodifiableList(m);
+			superID = id;
+			type = t;
+		}
+
+		@Override
+		public <S, E extends Exception> S accept(BaseASTVisitor<S, E> visitor) throws E { return visitor.visitNode(this);}
+	}
+
+	public static class FieldNode extends DecNode {
+		final String id;
+		FieldNode(String i, TypeNode t) {
+			id = i;
+			type = t;}
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class MethodNode extends DecNode {
+		final String id;
+		final TypeNode retType;
+		final List<ParNode> parlist;
+		final List<DecNode> declist;
+		final Node exp;
+		MethodNode(String i, TypeNode rt, List<ParNode> pl, List<DecNode> dl, Node e) {
+			id=i;
+			retType=rt;
+			parlist=Collections.unmodifiableList(pl);
+			declist=Collections.unmodifiableList(dl);
+			exp=e;
+		}
+
+		//void setType(TypeNode t) {type = t;}
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class ClassCallNode extends Node {
+		final String classId;
+		final String methodId;
+		final List<Node> arglist;
+		STentry entry;
+		int nl;
+		ClassCallNode(String cId, String mId, List<Node> p) {
+			classId = cId;
+			methodId = mId;
+			arglist = Collections.unmodifiableList(p);
+		}
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class NewNode extends Node {
+		final String classId;
+		final List<Node> arglist;
+		STentry entry;
+		int nl;
+		NewNode(String cId, List<Node> p) {
+			classId = cId;
+			arglist = Collections.unmodifiableList(p);
+		}
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class EmptyNode extends DecNode{
+		final String id;
+		STentry entry;
+		int nl;
+		EmptyNode(String i) {id = i;}
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class ClassTypeNode extends TypeNode {
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class MethodTypeNode extends TypeNode {
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class RefTypeNode extends TypeNode {
+		final String id;
+		RefTypeNode(String i) {id = i;}
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
+	public static class EmptyTypeNode extends TypeNode {
+
+		@Override
+		public <S,E extends Exception> S accept(BaseASTVisitor<S,E> visitor) throws E {return visitor.visitNode(this);}
+	}
+
 }
